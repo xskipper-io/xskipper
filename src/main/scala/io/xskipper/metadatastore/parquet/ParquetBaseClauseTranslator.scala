40c675cb68f73ac85ead1a3633a3936085313c91
Guy Khazma <guykhag@gmail.com>
2021-05-11 19:44:12 +0300

    add is not null to all clause translations
    Signed-off-by: Guy Khazma <guykhag@gmail.com>

--- src/main/scala/io/xskipper/metadatastore/parquet/ParquetBaseClauseTranslator.scala
+++ src/main/scala/io/xskipper/metadatastore/parquet/ParquetBaseClauseTranslator.scala
@@ -69,21 +69,21 @@ object ParquetBaseClauseTranslator extends ClauseTranslator {
               case LT => metadataCol < value
               case LTE => metadataCol <= value
             }
-            Some(expression.and(!isnull(metadataCol)))
+            Some(!isnull(metadataCol).and(expression))
           // checks if a list of values exists in the value list metadata
           // (used for equality checks)
           case ValueListClause(column, values, false) =>
             val mdColName = ParquetUtils.getColumnNameForCols(Seq(column), "valuelist")
-            Some(arrays_overlap(col(mdColName), lit(values)))
+            Some(!isnull(col(mdColName)).and(arrays_overlap(col(mdColName), lit(values)))
           // checks if the value list metadata contain values which
           // are different than the given list of values
           // (used for inequality checks)
           case ValueListClause(column, values, true) =>
             val mdColName = ParquetUtils.getColumnNameForCols(Seq(column), "valuelist")
-            Some(size(array_except(col(mdColName), lit(values))) > 0)
+            Some(!isnull(col(mdColName)).and(size(array_except(col(mdColName), lit(values))) > 0))
           case BloomFilterClause(column, values) =>
             val mdColName = ParquetUtils.getColumnNameForCols(Seq(column), "bloomfilter")
-            Some(bloomFilterUDF(values)(col(mdColName)))
+            Some(!isnull(col(mdColName)).and(bloomFilterUDF(values)(col(mdColName))))
           case _ =>
             None
         }
